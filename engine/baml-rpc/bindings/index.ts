27d7c9a04d7dc0b3549462c2d7af33470e2161d6
Chris Watts <chris@boundaryml.com>
2025-04-16 12:06:34 -0700

    feat: add baml-rpc bindings and package.json for rpc

---
+++ engine/baml-rpc/bindings/index.ts
@@ -0,0 +1,29 @@
+// Organization related exports
+export * from './Organization';
+export * from './CreateOrganizationRequest';
+export * from './CreateOrganizationResponse';
+export * from './GetOrganizationRequest';
+export * from './GetOrganizationResponse';
+export * from './UpdateOrganizationRequest';
+export * from './UpdateOrganizationResponse';
+
+// Project related exports
+export * from './Project';
+export * from './CreateProjectRequest';
+export * from './CreateProjectResponse';
+export * from './ListProjectsRequest';
+export * from './ListProjectsResponse';
+export * from './UpdateProjectRequest';
+export * from './UpdateProjectResponse';
+
+// Function related exports
+export * from './FunctionInput';
+export * from './FunctionSpan';
+export * from './ListFunctionSpansRequest';
+export * from './ListFunctionSpansResponse';
+
+// Webhook related exports
+export * from './PropelAuthWebhookRequest';
+export * from './PropelAuthWebhookResponse';
+export * from './StripeWebhookRequest';
+export * from './StripeWebhookResponse';
