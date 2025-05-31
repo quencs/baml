15591c7eb53eee6b91adb09ba9750177de68f56f
Sam Lijin <sam@boundaryml.com>
2025-05-30 17:13:08 -0700

    add working code

--- jetbrains/src/main/java/com/boundaryml/jetbrains_ext/BamlLanguageServerFactory.java
+++ jetbrains/src/main/java/com/boundaryml/jetbrains_ext/BamlLanguageServerFactory.java
@@ -3,6 +3,7 @@ package com.boundaryml.jetbrains_ext;
 import com.intellij.openapi.project.Project;
 import com.redhat.devtools.lsp4ij.LanguageServerFactory;
 import com.redhat.devtools.lsp4ij.client.LanguageClientImpl;
+import com.redhat.devtools.lsp4ij.client.features.LSPClientFeatures;
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,6 +12,14 @@ public class BamlLanguageServerFactory implements LanguageServerFactory {
     @Override
     public @NotNull StreamConnectionProvider createConnectionProvider(@NotNull Project project) {
         return new BamlLanguageServer();
+    }
+
+    @Override
+    public LSPClientFeatures createClientFeatures() {
+//        return null;
+        var features = new LSPClientFeatures();
+        features.setServerInstaller(new BamlLanguageServerInstaller()); // customize language server installer
+        return features;
     }
 //
 //    @Override // If you need to provide client specific features
@@ -24,3 +33,5 @@ public class BamlLanguageServerFactory implements LanguageServerFactory {
 //    }
 
 }
+
+
