2462fd781aae9fced2c42f02da07be1bdee95fd1
egol <egoriluk@gmail.com>
2025-06-09 22:34:23 -0700

    cleaned up code

--- bundle-hello-world/src/main.rs
+++ bundle-hello-world/src/main.rs
@@ -1,15 +1,12 @@
-// src/main.rs
 use futures_util::{SinkExt, StreamExt};
 use include_dir::{Dir, include_dir};
 use mime_guess::from_path;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::convert::Infallible;
 use std::fs;
-use std::path::PathBuf;
 use std::sync::Arc;
-use std::{convert::Infallible, ops::Range};
 use tokio::sync::RwLock;
-use tokio::sync::broadcast;
 use warp::{
     Filter,
     http::Response,
@@ -111,75 +108,8 @@ impl BamlState {
     }
 }
 
-async fn handle_ws_message(
-    msg: String,
-    state: Arc<RwLock<BamlState>>,
-    tx: broadcast::Sender<String>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let message: FrontendMessage = serde_json::from_str(&msg)?;
-
-    match message {
-        FrontendMessage::AddProject { root_path, files } => {
-            let mut state = state.write().await;
-            state.files = files;
-
-            // Process BAML files
-            if let Some(baml_content) = state.files.get("receipt.baml") {
-                // TODO: Parse BAML and validate
-                // TODO: Update diagnostics
-                // TODO: Send updates to frontend
-            }
-        }
-        FrontendMessage::ModifyFile {
-            root_path,
-            name,
-            content,
-        } => {
-            let mut state = state.write().await;
-            if let Some(content) = content {
-                state.files.insert(name, content);
-                // TODO: Revalidate BAML
-                // TODO: Update diagnostics
-                // TODO: Send updates to frontend
-            }
-        }
-        FrontendMessage::RemoveProject { root_path } => {
-            let mut state = state.write().await;
-            state.files.clear();
-            state.current_function = None;
-            state.diagnostics.clear();
-        }
-        FrontendMessage::SetFlashingRegions { spans } => {
-            // TODO: Handle flashing regions
-        }
-        FrontendMessage::SelectFunction {
-            root_path,
-            function_name,
-        } => {
-            let mut state = state.write().await;
-            state.current_function = Some(function_name);
-        }
-        FrontendMessage::UpdateCursor { cursor } => {
-            // TODO: Handle cursor updates
-        }
-        FrontendMessage::BamlSettingsUpdated { settings } => {
-            // TODO: Handle settings updates
-        }
-        FrontendMessage::RunTest { test_name } => {
-            // TODO: Handle test execution
-        }
-    }
-
-    Ok(())
-}
-
-async fn client_connection(
-    ws: warp::ws::WebSocket,
-    state: Arc<RwLock<BamlState>>,
-    tx: broadcast::Sender<String>,
-) {
+async fn client_connection(ws: warp::ws::WebSocket, state: Arc<RwLock<BamlState>>) {
     let (mut ws_tx, mut ws_rx) = ws.split();
-    let mut rx = tx.subscribe();
 
     // Load BAML files and send initial state
     {
@@ -188,15 +118,6 @@ async fn client_connection(
             eprintln!("Error loading BAML files: {}", e);
         }
     }
-
-    // Send initial port number
-    let port_msg = serde_json::json!({
-        "command": "port_number",
-        "content": {
-            "port": 3030
-        }
-    });
-    let _ = ws_tx.send(Message::text(port_msg.to_string())).await;
 
     // Send initial project state
     let state_read = state.read().await;
@@ -208,61 +129,18 @@ async fn client_connection(
         }
     });
     let _ = ws_tx.send(Message::text(add_project_msg.to_string())).await;
-
-    // Handle incoming messages
-    let state_clone = state.clone();
-    let tx_clone = tx.clone();
-    let handle_incoming = async move {
-        while let Some(result) = ws_rx.next().await {
-            match result {
-                Ok(msg) => {
-                    if let Ok(text) = msg.to_str() {
-                        if let Err(e) = handle_ws_message(
-                            text.to_string(),
-                            state_clone.clone(),
-                            tx_clone.clone(),
-                        )
-                        .await
-                        {
-                            eprintln!("Error handling message: {}", e);
-                        }
-                    }
-                }
-                Err(e) => {
-                    eprintln!("WebSocket error: {}", e);
-                    break;
-                }
-            }
-        }
-    };
-
-    // Handle outgoing messages
-    let handle_outgoing = async move {
-        while let Ok(msg) = rx.recv().await {
-            if ws_tx.send(Message::text(msg)).await.is_err() {
-                break;
-            }
-        }
-    };
-
-    tokio::select! {
-        _ = handle_incoming => {},
-        _ = handle_outgoing => {},
-    }
 }
 
 #[tokio::main]
 async fn main() {
-    let (tx, _rx) = broadcast::channel::<String>(16);
     let state = Arc::new(RwLock::new(BamlState::new()));
 
     // WebSocket handler
     let ws_route = warp::path("ws")
         .and(warp::ws())
         .map(move |ws: warp::ws::Ws| {
-            let tx = tx.clone();
             let state = state.clone();
-            ws.on_upgrade(move |socket| client_connection(socket, state, tx))
+            ws.on_upgrade(move |socket| client_connection(socket, state))
         });
 
     // Static file serving
