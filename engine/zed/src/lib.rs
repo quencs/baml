1835beab22311a512e49a2ccb0ccbb9bbfcceadb
egol <egoriluk@gmail.com>
2025-06-24 18:18:09 -0700

    fix formatting

--- engine/zed/src/lib.rs
+++ engine/zed/src/lib.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use zed_extension_api::{self as zed, settings::LspSettings, LanguageServerId, Result};
+use zed_extension_api::{self as zed, LanguageServerId, Result, settings::LspSettings};
 
 // Follows csharp extension as a template:
 // https://github.com/zed-extensions/csharp/blob/main/src/csharp.rs
