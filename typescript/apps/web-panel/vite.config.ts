6e151b93e0e9198eb6ac40e14ec5144ca7355c3c
Chris Watts <chris@boundaryml.com>
2025-06-27 18:27:40 -0700

    fix: issue with wasm build

--- typescript/apps/web-panel/vite.config.ts
+++ typescript/apps/web-panel/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
-const isWatchMode = process.argv.includes('--watch') || true;
+const isWatchMode = process.argv.includes('--watch');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
