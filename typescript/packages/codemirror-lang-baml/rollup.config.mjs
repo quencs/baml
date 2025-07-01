9ce1db8ec61b77e0cb122524b369292eea967a58
Chris Watts <chris@boundaryml.com>
2025-07-01 16:00:28 -0700

    bulid

--- typescript/packages/codemirror-lang-baml/rollup.config.mjs
+++ typescript/packages/codemirror-lang-baml/rollup.config.mjs
@@ -5,8 +5,19 @@ export default {
   input: 'src/index.ts',
   external: (id) => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
   output: [
-    { file: 'dist/index.cjs', format: 'cjs' },
-    { dir: './dist', format: 'es' },
+    { file: 'dist/index.cjs', format: 'cjs', exports: 'named' },
+    { file: 'dist/index.js', format: 'es' },
   ],
-  plugins: [lezer(), typescript()],
+  plugins: [
+    lezer(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationMap: true,
+      outDir: 'dist',
+      module: 'ESNext',
+      target: 'ES2020',
+      moduleResolution: 'node'
+    })
+  ],
 };
