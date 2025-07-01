bbe67306e7ff4b776c59b3d231b0fef534404446
Chris Watts <chris@boundaryml.com>
2025-06-30 21:50:26 -0700

    chore: rust fmt

--- engine/language_server/src/playground/playground_server.rs
+++ engine/language_server/src/playground/playground_server.rs
@@ -6,8 +6,10 @@ use tokio::sync::RwLock;
 /// Script that runs the playground server.
 /// On the input port
 use crate::playground::definitions::PlaygroundState;
-use crate::{playground::playground_server_helpers::create_server_routes, session::Session};
-use crate::playground::proxy::ProxyServer;
+use crate::{
+    playground::{playground_server_helpers::create_server_routes, proxy::ProxyServer},
+    session::Session,
+};
 
 #[derive(Debug, Clone)]
 pub struct PlaygroundServer {
@@ -26,7 +28,7 @@ impl PlaygroundServer {
         // Start the proxy server on a different port
         let proxy_port = port + 1; // Use playground port + 1 for proxy
         let proxy_server = ProxyServer::new(proxy_port);
-        
+
         // Spawn the proxy server in a separate task
         let proxy_handle = tokio::spawn(async move {
             if let Err(e) = proxy_server.start().await {
@@ -37,7 +39,7 @@ impl PlaygroundServer {
         // Start the main playground server
         tracing::info!("Starting main playground server on port {}", port);
         warp::serve(routes).run(([127, 0, 0, 1], port)).await;
-        
+
         // If we get here, the main server has stopped
         tracing::info!("Main playground server stopped");
 
