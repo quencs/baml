c8fd18b143d19b235f44e707c2d4037e9895f4ad
Rahul Tiwari <jprrahultiwari@gmail.com>
2025-07-28 19:01:49 -0700

    fix: go panic (#2201)
    This was an interesting one!
    TODO: add more findings why it works on Mac and not on Linux
    <!-- ELLIPSIS_HIDDEN -->
    
    ----
    
    > [!IMPORTANT]
    > Fixes Go panic by adjusting memory allocation in `InvokeRuntimeCli`
    and updates Dockerfile for Linux compatibility.
    > 
    >   - **Go Panic Fix**:
    > - In `exports.go`, `InvokeRuntimeCli` function now allocates an extra
    slot in `arg_c_strings` array to prevent out-of-bounds access.
    > - Adjusts `defer` block to correctly free only initialized C strings.
    >   - **Dockerfile Update**:
    > - In `test-package.Dockerfile`, adds `--platform=linux/amd64` to
    `FROM` directive to ensure compatibility with Linux.
    > - Installs Go 1.22 and protobuf Go plugin for building Go
    applications.
    > 
    > <sup>This description was created by </sup>[<img alt="Ellipsis"
    src="https://img.shields.io/badge/Ellipsis-blue?color=175173">](https://www.ellipsis.dev?ref=BoundaryML%2Fbaml&utm_source=github&utm_medium=referral)<sup>
    for 6cbb8de5168206f1f5c2eb28381055c4064116a2. You can
    [customize](https://app.ellipsis.dev/BoundaryML/settings/summaries) this
    summary. It will automatically update as commits are pushed.</sup>
    
    <!-- ELLIPSIS_HIDDEN -->

--- engine/language_client_go/baml_go/exports.go
+++ engine/language_client_go/baml_go/exports.go
@@ -42,13 +42,14 @@ func BamlVersion() string {
 }
 
 func InvokeRuntimeCli(args []string) (int, error) {
-	arg_c_strings := make([]*C.char, len(args))
+	arg_c_strings := make([]*C.char, len(args)+1)
 	for i, arg := range args {
 		arg_c_strings[i] = C.CString(arg)
 	}
+	
 	defer func() {
-		for _, arg_c_string := range arg_c_strings {
-			C.free(unsafe.Pointer(arg_c_string))
+		for i := 0; i < len(args); i++ {
+			C.free(unsafe.Pointer(arg_c_strings[i]))
 		}
 	}()
 
